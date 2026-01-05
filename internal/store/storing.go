bcdd1b5f7f412f6da31e79e317939bc6b9e43d3f
Abu <abdullahfakrudeen2020@gmail.com>
2026-01-05 19:08:58 +0530

    Created a store file
    Signed-off-by: Abu <abdullahfakrudeen2020@gmail.com>

--- internal/store/storing.go
+++ internal/store/storing.go
@@ -1,2 +1,6 @@
 package store
 
+/*
+ * This file will have an integration with GitHub to create a private repo on the user's account. Whenever a password is stored,
+ * it will be saved in the repo in a JSON file in an encrypted format.
+ */
