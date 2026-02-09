f87d1314c5b7ebaa7cfeaaaed9622fae3441b48e
Abu <abdullahfakrudeen2020@gmail.com>
2026-02-09 14:21:30 +0530

    Final commit for release: Genesis
    Signed-off-by: Abu <abdullahfakrudeen2020@gmail.com>

--- cmd/version.go
+++ cmd/version.go
@@ -21,7 +21,7 @@ Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		version := "0.1.0"
+		version := "v0.1.0"
 		fmt.Println("genp "+version)
 	},
 }
